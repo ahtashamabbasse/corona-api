9652e4d020494b56219855127352137806cc3f06
Ahtasham Abbas <ahtasham.abbas@northbaysolutions.net>
2020-03-21 15:49:37 +0500

    website url added in mail url

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const CovidController = require('../controller/covidController');
 const covidController=new CovidController();
 
-router.get('/', covidController.getTotalCases);
+router.get('/', (req,res)=>res.redirect('http://coronacases.info'));
+router.get('/total', covidController.getTotalCases);
 router.get('/countries', covidController.getCountries);
 router.get('/countries/:country', covidController.getCountry);
 router.get('/countries/sort/:column/:order', covidController.getSortedCountries);
